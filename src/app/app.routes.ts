f16267e581a748eaf89c820cb8078cae5eccd9cc
Julian Suarez <julianjose2511@gmail.com>
2024-04-27 23:54:06 -0400

    creando page login y registro para apk ionic angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,12 @@ export const routes: Routes = [
     path: 'examen',
     loadComponent: () => import('./examen/examen.page').then( m => m.ExamenPage)
   },
+  {
+    path: 'login-user',
+    loadComponent: () => import('./login-user/login-user.page').then( m => m.LoginUserPage)
+  },
+  {
+    path: 'register-user',
+    loadComponent: () => import('./register-user/register-user.page').then( m => m.RegisterUserPage)
+  },
 ];
